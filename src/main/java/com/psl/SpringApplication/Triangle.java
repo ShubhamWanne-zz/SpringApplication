89426458c169aefd6d1b3208a6e1f15e2cec2bab
shubham <shubham@idea-PC>
2018-04-22 09:10:16 +0530

    after completion of 1 learning module

--- src/main/java/com/psl/SpringApplication/Triangle.java
+++ src/main/java/com/psl/SpringApplication/Triangle.java
@@ -1,34 +1,60 @@
 package com.psl.SpringApplication;
 
+import java.util.List;
+import java.util.Map;
+
 public class Triangle {
-	private String type;
-	private int height;
+	private Point a;
+	private Point b;
+	private Point c;
+	
+	private List<Point> points;
+	private Map<Integer,Point> points_map;
 	
 	
-	
-	public Triangle(String type, int height) {
-		super();
-		this.type = type;
-		this.height = height;
+	public Map<Integer, Point> getPoints_map() {
+		return points_map;
 	}
 
-	public int getHeight() {
-		return height;
+	public void setPoints_map(Map<Integer, Point> points_map) {
+		this.points_map = points_map;
 	}
 
-	public void setHeight(int height) {
-		this.height = height;
+	public List<Point> getPoints() {
+		return points;
 	}
 
-	public String getType() {
-		return type;
+	public void setPoints(List<Point> points) {
+		this.points = points;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public Point getA() {
+		return a;
+	}
+
+	public void setA(Point a) {
+		this.a = a;
+	}
+
+	public Point getB() {
+		return b;
+	}
+
+	public void setB(Point b) {
+		this.b = b;
+	}
+
+	public Point getC() {
+		return c;
+	}
+
+	public void setC(Point c) {
+		this.c = c;
 	}
 
 	public void draw(){
-		System.out.println(getType()+" Traingle drawn of height "+getHeight());
+		System.out.println("Created triangle with; \na = "+a+", \nb = "+b+", \nc = "+c);
+		System.out.println("Create triangle with List of points : "+points);
+		System.out.println("Create triangle with Map of points : "+points_map);
 	}
 }
